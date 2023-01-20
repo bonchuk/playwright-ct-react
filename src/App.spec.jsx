a563e4b9aef91af44c315ae5ef4e9b94968d0ed1
Oleh Bonchuk <bonchuk@MacBook-Air-Oleg.local>
2023-01-20 16:09:12 +0200

    component test

---
+++ src/App.spec.jsx
@@ -0,0 +1,14 @@
+import { test, expect } from '@playwright/experimental-ct-react'
+import App from './App'
+
+test('First Test of component', async ({ mount }) => {
+
+  const component = await mount(<App />);
+
+  await expect(component).toContainText("Learn React");
+
+  expect(await component.locator('.App-logo').screenshot({
+    animations: "disabled"
+  })).toMatchSnapshot()
+
+})
