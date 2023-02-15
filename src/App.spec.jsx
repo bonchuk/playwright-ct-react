07cddec1ba4033cd799dba8e7028374b7c55d5c2
Oleh Bonchuk <bonchuk@MacBook-Air-Oleg.local>
2023-02-15 13:13:48 +0200

    first commit

--- src/App.spec.jsx
+++ src/App.spec.jsx
@@ -2,7 +2,6 @@ import { test, expect } from '@playwright/experimental-ct-react'
 import App from './App'
 
 test('First Test of the component', async ({ mount }) => {
-
   const component = await mount(<App />);
 
   await expect(component).toContainText("Learn React");
