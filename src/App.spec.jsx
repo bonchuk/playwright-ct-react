3ceda8f5b3ca1b3a2289effffdc8ee4e3de41525
Oleh Bonchuk <bonchuk@MacBook-Air-Oleg.local>
2023-02-23 14:30:08 +0200

    using css modules for react component

--- src/App.spec.jsx
+++ src/App.spec.jsx
@@ -1,4 +1,5 @@
 import { test, expect } from '@playwright/experimental-ct-react'
+import Timer from './Timer';
 import App from './App'
 
 test('First Test of the component', async ({ mount }) => {
@@ -6,8 +7,10 @@ test('First Test of the component', async ({ mount }) => {
 
   await expect(component).toContainText("Learn React");
 
+  await component.locator('.App-logo').waitFor({ state: "visible" })
+
   expect(await component.locator('.App-logo').screenshot({
-    animations: "disabled"
+    animations: 'disabled'
   })).toMatchSnapshot(
     {
       maxDiffPixels: 5
@@ -15,4 +18,12 @@ test('First Test of the component', async ({ mount }) => {
   )
 
   await component.screenshot({ path: 'screenshot.png' });
+})
+
+test.only('testing useEffect', async ({ mount }) => {
+  const component = await mount(<Timer/>);
+
+  await expect(component).toContainText("I have rendered 4 times!");
+  await component.waitFor();
+  await component.screenshot({ path: 'Timer_screenshot.png' });
 })
